da5753bf2b15485a551711046ec10b954de3de97
apoorv <apoorv@xample.com>
2021-09-07 22:03:46 +0530

    add signin post method

--- server/router/auth.js
+++ server/router/auth.js
@@ -6,26 +6,63 @@ router.get("/", (req, res) =>{
     res.send("Hi there, i am rourter");
 })
 
-router.post("/register", (req, res) =>{
+router.post("/register", async(req, res) =>{
     const {name, email, phone, work, password, cpassword} = req.body;
 
     if(!name || !email || !phone || !work || !password || !cpassword){
         res.status(422).send("Fill data properly!");
     }
 
-    User.findOne({email}).then((userExit) =>{
-        if(userExit)
-            return res.status(422).json({error:"Email already exist!"});
-        else{
-            const user = new User({name, email, phone, work, password, cpassword});
+    // User.findOne({email}).then((userExit) =>{
+    //     if(userExit)
+    //         return res.status(422).json({error:"Email already exist!"});
+    //     else{
+    //         const user = new User({name, email, phone, work, password, cpassword});
 
-            user.save().then(() =>{
-                return res.status(201).json({message:"You have registered successfully.."});
-            }).catch((e) => res.status(500).json({error:"Registeration failed!"}));
-        }
+    //         user.save().then(() =>{
+    //             return res.status(201).json({message:"You have registered successfully.."});
+    //         }).catch((e) => res.status(500).json({error:"Registeration failed!"}));
+    //     }
             
-    }).catch((e) => res.status(500).json({error:"failed to registered!"}));
+    // }).catch((e) => res.status(500).json({error:"failed to registered!"}));
 
+    try {
+
+        const userExist = await User.findOne({email});
+
+        if(userExist){
+            return res.status(422).json({error:"Email already exist!"});
+        }
+
+        const userResponse = new User({name, email, phone, work, password, cpassword});
+
+        await userResponse.save();
+
+        return res.status(201).json({message:"You have registered successfully.."});
+
+    } catch (error) {
+        res.status(500).json({error:"failed to registered!"});
+    }
+
+})
+
+router.post("/signin", async(req, res) =>{
+    const {email, password} = req.body;
+    if(!email || !password)
+        res.status(422).send("Fill data properly!");
+
+    try {
+
+        const userExist = await User.findOne({email});
+        if(!userExist)
+            res.status(400).json({error:"Invalid credentials!"});
+        else{
+            res.json({message: "Login successful..."});
+        }
+
+    } catch (error) {
+        res.status(500).json({error:"Internal server error!"});
+    }
 })
 
 module.exports = router;
