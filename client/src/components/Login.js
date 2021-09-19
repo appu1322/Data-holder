e4302abec4a90a4a5f8ee92aa5809a4c86abd38a
apoorv <apoorv@xample.com>
2021-09-19 23:29:38 +0530

    add signin and signup page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
+import {NavLink} from 'react-router-dom'
+import login from '../images/login.png'
 
 export default class Login extends Component {
     render() {
         return (
-            <div>
-                This is an Login page
+            <div className="container my-5">
+                <section className="signin-session">
+                    <div className="row mx-2">
+
+                        <div className="col-md-6 signin-form mt-5 mb-2">
+                            <img src={login} className="d-block w-75 mx-5 mt-5" alt="Login"/>
+                            <NavLink to="/signup" className="d-block text-center mb-3">Create an Account</NavLink>
+
+                        </div>
+                        <div className="col-md-6 signin-form mt-5 mb-2">
+                            <h2 className="text-center">Login form</h2>
+                            <form className="mt-5">
+                                <div className="mb-3">
+                                    <label htmlFor="emal" className="form-label">Email address</label>
+                                    <input type="email" className="form-control" id="emal" aria-describedby="emailHelp"/>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="password" className="form-label">Password</label>
+                                    <input type="password" className="form-control" id="password"/>
+                                </div>
+                                <div className="text-center">
+                                    <button type="submit" className="btn btn-success btn-lg">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+                </section>
             </div>
         )
     }
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,10 +1,78 @@
 import React, { Component } from 'react'
+import { NavLink } from "react-router-dom";
+import signupImg from '../images/form.png'
 
 export default class Signup extends Component {
     render() {
         return (
-            <div>
-                This is an signup page
+            <div className="container py-5">
+                <section className="signup-session">
+                    <h2 className="text-center">Signup Form</h2>
+                    <div className="row mx-2">
+
+                        <div className="col-md-6 signup-form mt-5 mb-2">
+                        <form>
+                            <div className="mb-3">
+                                <label htmlFor="name" className="form-label">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
+                                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                                    </svg> Name
+                                </label>
+                                <input type="text" className="form-control" id="name" aria-describedby="emailHelp" autoComplete="off"/>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="email" className="form-label">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16">
+                                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
+                                </svg> Email address
+                                </label>
+                                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" autoComplete="off"/>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="phone" className="form-label">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-phone" viewBox="0 0 16 16">
+                                    <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
+                                    <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                                </svg> Phone Number
+                                </label>
+                                <input type="text" className="form-control" id="phone" aria-describedby="emailHelp" autoComplete="off"/>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="profession" className="form-label">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bar-chart-steps" viewBox="0 0 16 16">
+                                    <path d="M.5 0a.5.5 0 0 1 .5.5v15a.5.5 0 0 1-1 0V.5A.5.5 0 0 1 .5 0zM2 1.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5v-1zm2 4a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-1zm2 4a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1-.5-.5v-1zm2 4a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-1z"/>
+                                </svg> Profession
+                                </label>
+                                <input type="text" className="form-control" id="profession" aria-describedby="emailHelp" autoComplete="off"/>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="password" className="form-label">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-lock-fill" viewBox="0 0 16 16">
+                                    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
+                                </svg> Password
+                                </label>
+                                <input type="password" className="form-control" id="password" autoComplete="off"/>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="confirm-password" className="form-label">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-lock" viewBox="0 0 16 16">
+                                    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
+                                </svg> Confire Password
+                                </label>
+                                <input type="password" className="form-control" id="confirm-password" autoComplete="off"/>
+                            </div>
+                            <div className="text-center">
+                            <button className="btn btn-success">Rgister</button>
+                            </div>
+                            </form>
+                        </div>
+
+                        <div className="col-md-6 mt-5">
+                            <img src={signupImg} className="d-block w-75 m-5" alt="Signup"/>
+                            <NavLink to="/login" className="d-block text-center">I have already signup</NavLink>
+                        </div>
+                    </div>
+                </section>
             </div>
         )
     }
